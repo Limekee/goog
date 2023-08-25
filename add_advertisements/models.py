9e6425cfe18ce02ebf5ffeed22cc8557ad4f5720
Егор <esyropatov@gmail.com>
2023-08-25 16:22:16 +0500

    first commit

---
+++ add_advertisements/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Advertisement(models.Model):
+    id = models.CharField("id", max_length=64, primary_key=True)
+    title=models.CharField('заголовок', max_length=128)
+    description=models.TextField('описание')
+    price=models.DecimalField('цена', max_digits=10, decimal_places=2)
+    auction=models.BooleanField('торг', help_text='Отметьте, если торг уместен')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return '<Advertisement: Advertisement(id={}, title={}, price={})>.'.format(self.id, self.title, self.price)
+    class Meta:
+        db_table = "advertisements"
